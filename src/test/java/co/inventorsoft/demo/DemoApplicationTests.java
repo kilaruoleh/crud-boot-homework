8fa68ffc9af1a8b3c71ece1d81e312c39594adf9
Oleh Kilaru <kilaruoleh@gmail.com>
2020-10-16 09:50:19 +0300

    made homework

---
+++ src/test/java/co/inventorsoft/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.inventorsoft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
