7df4371da9d12faa64aba866e2ccf5996ae39b9d
Oleh Kilaru <kilaruoleh@gmail.com>
2020-10-18 22:00:10 +0300

    changed Controller to RestController

--- src/main/java/co/inventorsoft/demo/controllers/UsersController.java
+++ src/main/java/co/inventorsoft/demo/controllers/UsersController.java
@@ -4,18 +4,14 @@ import co.inventorsoft.demo.dao.UsersDao;
 import co.inventorsoft.demo.forms.UserForm;
 import co.inventorsoft.demo.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class UsersController {
 
     @Autowired
